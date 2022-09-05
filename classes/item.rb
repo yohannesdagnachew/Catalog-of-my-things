5a8c0f53b1cb3dd4c8ad689d637dc102038b5638
rich <adukyerer@gmail.com>
2022-09-05 13:04:32 +0000

    add item class

---
+++ classes/item.rb
@@ -0,0 +1,37 @@
+require 'date'
+
+class Item
+    attr_accessor  :genre, :author, :label
+    attr_reader :publish_date, :archived
+    def initialize(date)
+    @publish_date =date
+    @archived=false
+    @id = rand(1..1000)
+    end
+   
+    def can_be_archived?
+     Date.today - Date.parse(@publish_date) > 3650
+    end
+
+    def move_to_archive
+     can_be_archived? ? @archived=true : @archived=false
+    end
+ 
+    def add_genre=(genre)
+        @genre = genre
+        genre.add_item(self) unless genre.items.include?(self)
+    end
+    
+    def add_author=(author)
+        @author = author
+        author.add_item(self) unless author.items.include?(self)
+    end
+
+    def add_label=(label)
+        @label = label
+        label.add_item(self) unless label.items.include?(self)
+    end
+end
+
+
+
