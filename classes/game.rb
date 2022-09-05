4e68cbd382acb1146180dae149dabcdab28625b9
rich <adukyerer@gmail.com>
2022-09-05 22:03:00 +0000

    add game class

---
+++ classes/game.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative 'item'
+require 'date'
+
+# Game class
+class Game < Item
+  attr_reader :multiplayer, :last_played_at
+
+  def initialize(date, multiplayer, last_played_at)
+    super(date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && Date.today - Date.parse(@last_played_at) > 1464
+  end
+end
