8820e01eba4c9d46fef917696d179b545abe399a
Felix Ouma <fatonoh@gmail.com>
2022-09-05 22:44:22 +0300

    Add blueprint for objects of class Book

---
+++ classes/book.rb
@@ -0,0 +1,20 @@
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
+end
+
+book = Book.new('2018-06-30', 'Longhorn', 'bad')
+p book.move_to_archive
